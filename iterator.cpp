e017df3ab2f8a2aacc174d902d03d126f1d3aa08
ivansindacco <56919658+ivansindacco@users.noreply.github.com>
2024-05-19 16:43:39 +0300

    Add files via upload

---
+++ iterator.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <vector>
+
+int main() {
+    std::vector<int> numbers = { 1, 2, 3, 4, 5 };
+
+    // Умножаем каждый элемент на 2
+    for (std::vector<int>::iterator it = numbers.begin(); it != numbers.end(); ++it) {
+        *it *= 2;
+    }
+
+    // Выводим результат
+    for (const auto& num : numbers) {
+        std::cout << num << " ";
+    }
+
+    return 0;
+}
